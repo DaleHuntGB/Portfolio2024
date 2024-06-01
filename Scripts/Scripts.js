e7ca42da27ec9d1e4b579c6a616a98cf90960c86
Dale Hunt <dalehunt@gmail.com>
2024-06-01 16:27:36 +0100

    Developed Portfolio Containers.

--- Scripts/Scripts.js
+++ Scripts/Scripts.js
@@ -1,9 +1,9 @@
-var Proj1 = document.getElementById("Proj1");
-var Proj2 = document.getElementById("Proj2");
-var Proj3 = document.getElementById("Proj3");
-var Proj4 = document.getElementById("Proj4");
-var Proj5 = document.getElementById("Proj5");
-var Proj6 = document.getElementById("Proj6");
+var Proj1 = document.getElementById("Proj1_Container");
+var Proj2 = document.getElementById("Proj2_Container");
+var Proj3 = document.getElementById("Proj3_Container");
+var Proj4 = document.getElementById("Proj4_Container");
+var Proj5 = document.getElementById("Proj5_Container");
+var Proj6 = document.getElementById("Proj6_Container");
 
 Proj1.addEventListener("click", lambda => ExpandDropdown("Proj1"));
 Proj2.addEventListener("click", lambda => ExpandDropdown("Proj2"));
@@ -16,15 +16,18 @@ function ExpandDropdown(ID)
 {
     var ParagraphContainer = document.getElementById(ID + "_Content");
     var DivContainer = document.getElementById(ID);
+    var Arrow = document.getElementById(ID + "_Arrow");
     
     if (ParagraphContainer.style.display == "block")
     {
         ParagraphContainer.style.display = "none";
         DivContainer.style.padding = "0em";
+        Arrow.style.transform = "rotate(0deg)";
     }
     else
     {
         ParagraphContainer.style.display = "block";
         DivContainer.style.padding = "1em";
+        Arrow.style.transform = "rotate(180deg)";
     }
 }
